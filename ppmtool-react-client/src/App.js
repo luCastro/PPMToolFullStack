bff08541c2d7201b63aae4dc4161e097c70d7c57
luCastro <lucastro.marquise@gmail.com>
2020-06-02 16:35:01 -0700

    Project Item Create Button

--- ppmtool-react-client/src/App.js
+++ ppmtool-react-client/src/App.js
@@ -3,13 +3,24 @@ import "./App.css";
 import Dashboard from "./components/Dashboard";
 import Header from "./components/layout/Header";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "font-awesome/css/font-awesome.min.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import AddProject from "./components/projects/AddProject";
+
+
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Dashboard />
-    </div>
+
+    <Router>
+
+      <div className="App">
+        <Header />
+        <Dashboard />
+        <Route exact path="/addProject" component={AddProject} />
+      </div>
+
+    </Router>
   );
 }
 
