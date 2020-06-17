2b62b2b67ce5ff713a5e3c08bf8c721231035502
luCastro <lucastro.marquise@gmail.com>
2020-06-17 11:51:42 -0700

    Created redux store

--- ppmtool-react-client/src/App.js
+++ ppmtool-react-client/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import "./App.css";
 import Dashboard from "./components/Dashboard";
 import Header from "./components/layout/Header";
@@ -6,22 +6,23 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "font-awesome/css/font-awesome.min.css";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import AddProject from "./components/projects/AddProject";
+import {Provider} from "react-redux";
+import store from "./store";
 
-
-
-function App() {
-  return (
-
-    <Router>
-
-      <div className="App">
-        <Header />
-        <Route exact path="/dashboard" component={Dashboard} />
-        <Route exact path="/addProject" component={AddProject} />
-      </div>
-
-    </Router>
-  );
+class App extends Component {
+    render() {
+      return(
+        <Provider store={store}>
+          <Router>
+            <div className="App">
+              <Header />
+              <Route exact path="/dashboard" component={Dashboard} />
+              <Route exact path="/addProject" component={AddProject} />
+            </div>
+          </Router>
+        </Provider>
+      );
+    }
 }
 
 export default App;
